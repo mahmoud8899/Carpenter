87d148399438ae23f78c2d075aa7bb4a44646ec9
mahmoud.talat899@gmail.com <mahmoud.talat899@gmail.com>
2023-06-27 19:52:13 +0200

    update

--- src/server/Render.js
+++ src/server/Render.js
@@ -46,6 +46,15 @@ ${helmet.link.toString()}
 </script>
 
 
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-PG6JSZL');</script>
+<!-- End Google Tag Manager -->
+
+
 
 
 </head>
@@ -56,6 +65,7 @@ ${helmet.link.toString()}
     <script src="/client/bundle.js" defer ></script>
     <!-- Google tag (gtag.js) -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11228036732"></script>
+
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
@@ -63,6 +73,11 @@ ${helmet.link.toString()}
 
   gtag('config', 'AW-11228036732');
 </script>
+
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PG6JSZL"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 
     
 </body>
